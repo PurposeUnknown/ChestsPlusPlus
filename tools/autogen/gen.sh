196d3cdc0201e403b997dc6d686e142bc4f65ce9
Lucia Smith <lucia@luciaesmith.com>
2024-05-28 20:10:24 -0400

    Fix broken sorts.

--- tools/autogen/gen.sh
+++ tools/autogen/gen.sh
@@ -1,6 +1,6 @@
-export CLONE_LOCATION="/tmp/plugin"
-rm -rf $CLONE_LOCATION
-git clone https://github.com/ChestsPlusPlus/ChestsPlusPlus.git $CLONE_LOCATION
+export CLONE_LOCATION="/mnt/md0/home/friedmicro/dev/ChestsPlusPlus"
+# rm -rf $CLONE_LOCATION
+# git clone https://github.com/ChestsPlusPlus/ChestsPlusPlus.git $CLONE_LOCATION
 
 export TAG_PATH="ChestsPlusPlus_Main/src/main/java/com/jamesdpeters/minecraft/chests/filters/ChestsPlusPlusTag.java"
 export ITEM_TYPE_PATH="ChestsPlusPlus_Main/src/main/java/com/jamesdpeters/minecraft/chests/filters/ItemTypeUtil.java"
